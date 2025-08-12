3c5117a2aff50b3f24d3b2db0ca5b97db7dc6a35
User <User@GORENT-PC>
2025-08-12 08:13:50 +0200

    -

--- Controllers/WorkerController.cs
+++ Controllers/WorkerController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WorkConnect.Models; // Make sure this is included
+using System.Collections.Generic;
 
 namespace WorkConnect.Controllers
 {
@@ -6,7 +8,13 @@ namespace WorkConnect.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var workers = new List<Worker>
+            {
+                new Worker { FullName = "John Doe", ServiceType = "Plumbing", Location = "NYC", Languages = "English", Experience = "5 years", Phone = "1234567890", PhotoUrl = "john.jpg" },
+                new Worker { FullName = "Jane Smith", ServiceType = "Cleaning", Location = "Boston", Languages = "English, Spanish", Experience = "3 years", Phone = "9876543210", PhotoUrl = "jane.jpg" }
+            };
+
+            return View(workers);
         }
     }
 }
