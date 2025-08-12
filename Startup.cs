87026f62ed59832e3f1e50c3a79fa5bcd4e263cd
User <User@GORENT-PC>
2025-08-12 07:40:08 +0200

    Added the correct namespace in the startup file

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,26 @@
-﻿namespace WorkConnect
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using WorkConnect.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace WorkConnect
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
     }
 }
+
+    
+
